fd4ce26fb7293fcf383729292ac6cebfb85fe2b3
Paul Jacks <Rebstorm@users.noreply.github.com>
2023-10-03 12:39:10 +0200

    swap

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -27,7 +27,7 @@ export default function App({ Component, route, ...rest }: AppProps) {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link
-          href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;700&display=auto"
+          href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;700&display=swap"
           rel="stylesheet"
         />
         <title>jacks.se</title>
