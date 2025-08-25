874ecf1bcffe378e64e62cba56a27d02c022e63c
Paul Jacks <Rebstorm@users.noreply.github.com>
2025-08-25 12:06:01 +0200

    updating layouts

--- routes/experiments/_layout.tsx
+++ routes/experiments/_layout.tsx
@@ -1,20 +1,18 @@
 import {PageProps} from "$fresh/server.ts";
-import {Head} from "$fresh/runtime.ts";
-import {getCSSPathPrefix} from "../../utils/css/pathPrefix.ts";
+import {Head, asset} from "$fresh/runtime.ts";
 import {TITLE} from "../../constants/meta.ts";
 
 export default function Layout({ Component, state, ...rest }: PageProps) {
-  const pathPrefix = getCSSPathPrefix(rest.url.pathname);
 
   return (
     <>
       <Head>
         <link
           rel="preload"
-          href={`${pathPrefix}css/experiments.css`}
+          href={asset('/css/experiments.css')}
           as="style"
         />
-        <link rel="stylesheet" href={`${pathPrefix}css/experiments.css`} />
+        <link rel="stylesheet" href={asset('/css/experiments.css')} />
         <title> Experiments | {TITLE}</title>
       </Head>
       <Component />
