640c750d162c8840f191fc7b91ee268ecdb40667
Paul Jacks <Rebstorm@users.noreply.github.com>
2025-08-25 12:08:43 +0200

    that should remove the last of the bad code

--- routes/experiments/battery-rush.tsx
+++ routes/experiments/battery-rush.tsx
@@ -1,24 +1,19 @@
 import {HandlerContext, PageProps} from "$fresh/server.ts";
 import BatteryRush from "../../islands/BatteryRush/index.tsx";
-import {Head} from "$fresh/runtime.ts";
-import {getCSSPathPrefix} from "../../utils/css/pathPrefix.ts";
-import {
-    getHighscore,
-    HighscoreUser,
-} from "../../server/highscore/highscore.ts";
+import {asset, Head} from "$fresh/runtime.ts";
+import {getHighscore, HighscoreUser,} from "../../server/highscore/highscore.ts";
 import {TITLE} from "../../constants/meta.ts";
 
 export default function Home(props: PageProps<HighscoreUser[]>) {
-    const pathPrefix = getCSSPathPrefix(props.url.pathname);
 
     return (
         <div className={'center'}>
             <Head>
-                <link rel="preload" href={`${pathPrefix}css/flappy.css`} as="style"/>
-                <link rel="stylesheet" href={`${pathPrefix}css/flappy.css`}/>
+                <link rel="preload" href={asset('/css/flappy.css')} as="style"/>
+                <link rel="stylesheet" href={asset('/css/flappy.css')}/>
 
-                <link rel="preload" href={`${pathPrefix}css/battery-rush.css`} as="style"/>
-                <link rel="stylesheet" href={`${pathPrefix}css/battery-rush.css`}/>
+                <link rel="preload" href={asset('/css/battery-rush.css')} as="style"/>
+                <link rel="stylesheet" href={asset('/css/battery-rush.css')}/>
 
                 <title>Battery Rush | {TITLE}</title>
             </Head>
--- routes/experiments/flappy.tsx
+++ routes/experiments/flappy.tsx
@@ -1,21 +1,16 @@
 import {HandlerContext, PageProps} from "$fresh/server.ts";
 import GameWindow from "../../islands/Flappy/GameWindowProps.tsx";
-import {Head} from "$fresh/runtime.ts";
-import {getCSSPathPrefix} from "../../utils/css/pathPrefix.ts";
-import {
-    getHighscore,
-    HighscoreUser,
-} from "../../server/highscore/highscore.ts";
+import {asset, Head} from "$fresh/runtime.ts";
+import {getHighscore, HighscoreUser,} from "../../server/highscore/highscore.ts";
 import {TITLE} from "../../constants/meta.ts";
 
 export default function Home(props: PageProps<HighscoreUser[]>) {
-    const pathPrefix = getCSSPathPrefix(props.url.pathname);
 
     return (
         <>
             <Head>
-                <link rel="preload" href={`${pathPrefix}css/flappy.css`} as="style"/>
-                <link rel="stylesheet" href={`${pathPrefix}css/flappy.css`}/>
+                <link rel="preload" href={asset('/css/flappy.css')} as="style"/>
+                <link rel="stylesheet" href={asset('/css/flappy.css')}/>
 
                 <title>Flappy | {TITLE}</title>
             </Head>
